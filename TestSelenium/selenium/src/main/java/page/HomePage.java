3ff01cf8bf0ba8154b42e2f8945562c0900202e6
Hoa568 <tong.hoa@amaris.com>
2024-11-19 15:52:32 +0700

    Update HomePage.java

--- TestSelenium/selenium/src/main/java/page/HomePage.java
+++ TestSelenium/selenium/src/main/java/page/HomePage.java
@@ -5,73 +5,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import utils.DateUtils;
 public class HomePage extends BasePage {
-
-    String todayDate = DateUtils.getTodayDate();
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath = "//button[text()='Accept All Cookies']")
-    private WebElement acceptCookies;
+    @FindBy(cssSelector("div.orangehrm-login-form > form > div:nth-child(2) > div > div:nth-child(2) > input"));
+    private WebElement Username;
 
-    @FindBy(xpath = "//input[@name='flight_origin']" )
-    private WebElement inputFlightOrigin;
+    @FindBy(cssSelector("div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input" ));
+    private WebElement Password;
 
-    @FindBy(xpath = "//input[@name='flight_destiny']")
-    private WebElement inputFlightDestiny;
+    @FindBy(cssSelector("div.oxd-form-actions.orangehrm-login-action > button"))
+    private WebElement buttonLogin;
 
-    @FindBy(xpath = "//span[text()='Round trip']")
-    private WebElement selectTrip;
+    @FindBy(cssSelector("header > div.oxd-topbar-header > div.oxd-topbar-header-userarea > ul > li > span > i"))
+    private WebElement LogOutFrame;
 
-    @FindBy(xpath = "//span[text()='One way']")
-    private WebElement optionOneWay;
-
-    @FindBy(xpath = "//label[text()='Departure date']")
-    private WebElement selectDeparterDate;
-
-    @FindBy(xpath = "//div[@data-function='cal-start']")
-    private WebElement calendar;
-
-    @FindBy(xpath = "//button[@id='buttonSubmit1']")
-    private WebElement buttonSearch;
-
-    @FindBy(xpath = "//button[text()='Continue with my selection']")
-    private WebElement confirmSelection;
+    @FindBy(cssSelector("header > div.oxd-topbar-header > div.oxd-topbar-header-userarea > ul > li > ul > li:nth-child(4) > a"))
+    private WebElement LogOutButton;
     
-
-    public void acceptCookies() {
-        waitForVisibility(acceptCookies);
-        click(acceptCookies);
-    }
-    public void inputFlightOrigin(String query) {
-        enterText(inputFlightOrigin,query);
-    }
-
-    public void inputFlightDestiny(String query){
-        enterText(inputFlightDestiny, query);
-    }
-
-    public void selectTrip(){
-        click(selectTrip);
-        click(optionOneWay);
-    }
-    public void chooseDateOnCalender(){
-        click(selectDeparterDate);
-        waitForVisibility(calendar);
-        WebElement todayElement = calendar.findElement(By.xpath("//a[text()='" + todayDate + "']"));
-        click(todayElement);
-    }
-
-    public void searchResouces(){
-        click(buttonSearch);
-    }
-
-    public void confirmSelection(){
-        click(confirmSelection);
-    }
 
 
         
